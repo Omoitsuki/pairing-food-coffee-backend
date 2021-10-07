96ec885c3413bfcff1fd8ddb3e0a905c796f403b
EC2 Default User <leandro83g@icloud.com>
2021-10-07 16:18:17 +0000

    feat: foods/show,index作成

---
+++ app/controllers/api/v1/foods_controller.rb
@@ -0,0 +1,32 @@
+class Api::V1::FoodsController < ApplicationController
+  
+  def index
+    if params[:coffee_id]
+      @coffee = Coffee.find_by_id(params[:coffee_id])
+      return render json: { status: 200 } if @coffee.nil?
+      
+      @foods = @coffee.matching_foods
+    else
+      @foods = Food.all
+    end
+    
+    render json: { 
+      status: 500, 
+      foods: @foods,
+    }
+  end
+  
+  def show
+    @food = Food.find_by_id(params[:id])
+    
+    return render json: { status: 500 } if @food.nil?
+    
+    @matching_coffees = @food.matching_coffees
+    
+    render json: { 
+      status: 200, 
+      food: @food,
+      coffees: @matching_coffees,
+    }
+  end
+end
